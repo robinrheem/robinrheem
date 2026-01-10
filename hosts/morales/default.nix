82c81f33520d3d37a1aa65e3b414d827faf2e4a6
Robin Rheem <robinrheem@gmail.com>
2026-01-10 03:16:48 -0800

    Bluetooth: use older mode

--- hosts/morales/default.nix
+++ hosts/morales/default.nix
@@ -38,6 +38,9 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.extraModprobeConfig = ''
+    options bluetooth disable_ertm=1
+  '';
 
   networking.hostName = "morales"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
